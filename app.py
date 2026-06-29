59f4bba16d49c8031a4cf4d11e2e98f3cb5fe7da
ayan65ty <s.ayantayo2003@gmail.com>
2026-06-29 12:27:24 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+name = input("what is your name?: ")
+age = input("how old are you?: ")
+
+age = int(age)
+age = age + 1
+
+print(f"Hello {name} !")
+print("HAPPY BIRTHDAY !")
+print(f"you are {age} years old")
